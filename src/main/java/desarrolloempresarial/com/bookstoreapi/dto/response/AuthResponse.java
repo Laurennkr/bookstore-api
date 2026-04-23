7d9c756a03cdc68998a00c7fcc59fb0f15162393
Lauren <164786167+Laurennkr@users.noreply.github.com>
2026-04-22 20:14:43 -0500

    feat: add auth DTOs and DuplicateResourceException

--- src/main/java/desarrolloempresarial/com/bookstoreapi/dto/response/AuthResponse.java
+++ src/main/java/desarrolloempresarial/com/bookstoreapi/dto/response/AuthResponse.java
@@ -1,4 +1,16 @@
 package desarrolloempresarial.com.bookstoreapi.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
+    private String token;
+    private Long expiresIn;
+    private String role;
 }
